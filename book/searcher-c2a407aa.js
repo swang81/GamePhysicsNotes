e2690b2e6b6048b619ccabd848989c581220098b
jack wang <swang81@icloud.com>
2026-01-07 18:38:02 +0800

    构建框架

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6ce54b2e.js',
+                path_to_root + 'searchindex-b57ebaee.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- src/SUMMARY.md
+++ src/SUMMARY.md
@@ -1,3 +1,22 @@
 # Summary
 
-- [Chapter 1](./chapter_1.md)
+[介绍](README.md)
+
+# 游戏物理专题
+
+- [数学物理基础](./physics_math.md)
+- [碰撞检测](./collision_detection.md)
+- [动力学与约束求解](./dynamics_constraints.md)
+- [高级物理模拟](./advanced_simulation.md)
+- [物理可视化与调试](./physics_visualization.md)
+
+# 学习日志
+
+- [Rust物理引擎探索](./rust_explorer.md)
+
+
+# 知识碎片
+
+
+
+
