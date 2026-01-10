2bfcf2e32cd3a6fb0b8dc0d5684a253c43d18fa9
jack wang <swang81@icloud.com>
2026-01-10 15:22:34 +0800

    Update documentation

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-61fafed4.js',
+                path_to_root + 'searchindex-ef2242e3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
